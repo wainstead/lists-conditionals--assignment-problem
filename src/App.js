a2951109af1be9922c21f0820d54f3225ddc8712
Steve Wainstead <swain@panix.com>
2018-11-08 17:51:38 -0500

    Implemented first item.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    strlength: 0,
+  };
 
+  strLengthChangeHandler = (event) => {
+    // I need a quick lookup for writing these things...
+    this.setState({strlength: event.target.value.length});
+  }
   render() {
-    // const mystyle = {
-    //   color: "red",
-    //   size: "12px"
-    // };
     return (
       <div className="App">
         <ol>
-          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+          <li className="struck">Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
           <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
           <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
           <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
@@ -20,8 +23,8 @@ class App extends Component {
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
         <hr/>
-        <input type="text"></input>
-        <p>This will contain the length count.</p>
+        <input type="text" onChange={this.strLengthChangeHandler}></input>
+        <p>Current length of the input: {this.state.strlength}</p>
       </div>
 
     );
