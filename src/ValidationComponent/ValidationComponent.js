e39e92a135879cf7a079201a411c2216f4a79516
Steve Wainstead <swain@panix.com>
2018-11-09 11:06:51 -0500

    Added ValidationComponent, added messaging

---
+++ src/ValidationComponent/ValidationComponent.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ValidationComponent.css';
+
+const validationComponent = (props) => {
+    //let isTextLongEnough = false;
+    let msg = "Text too short";
+    if (props.strlength >= 5) {
+        msg = "Text long enough"
+    }
+    return (
+        <div>
+            <p className="vcp">Hello {props.name}!</p>
+            <p className="vcp">The length of the string is {props.strlength}</p>
+            <p className="vcp">{msg}</p>
+        </div>
+    )
+}
+
+export default validationComponent;
